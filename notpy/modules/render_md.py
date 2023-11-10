4806f4f8d928fc17f286cd7b33071db03bb5431a
jontok <jonas@tokmaji.de>
2023-11-10 19:00:29 +0100

    fix(pdf-gerneration): create tmp dir if not exists

--- notpy/modules/render_md.py
+++ notpy/modules/render_md.py
@@ -1,3 +1,4 @@
+import os
 from markdown import markdown
 from markdown_pdf import MarkdownPdf, Section
 from modules.read_md import getCurrentMarkdown
@@ -8,5 +9,7 @@ def convertToPDF(work_dir, path):
     pdf = MarkdownPdf(toc_level=2)
     pdf.add_section(Section(markdown_str))
     pdf.meta["title"] = "Render"
+    if not os.path.isdir(work_dir + "/tmp"):
+        os.mkdir(work_dir + "/tmp")
     pdf.save(work_dir + "/tmp/" + filename + ".pdf")
     print("Rendered " + work_dir + "/tmp/" + filename + ".pdf")
